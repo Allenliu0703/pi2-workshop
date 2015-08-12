2b92550fdd4a98c5570b8ad9e740806b451f2cf1
Allenliu0703 <346607473@qq.com>
2015-08-12 04:56:08 -0700

    ad

---
+++ json_weather2.py
@@ -0,0 +1,10 @@
+import urllib.request, json
+
+url = "http://api.openweathermap.org/data/2.5/forecast/daily?cnt=7&units=meteric&mode=json&q=London"
+
+req = urllib.request.Request(url)
+forecast_string = urllib.request.urlopen(req).read()
+forecast_dict = json.loads(forecast_string.decode("UTF-8"))
+#print(urllib.request.urlopen(req).read())
+
+print(forecast_dict)
