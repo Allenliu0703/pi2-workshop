f1be42dd3eff4e5acee7f2eb7ecd71c2155ed48f
Allenliu0703 <346607473@qq.com>
2015-08-14 21:27:11 -0700

    d

---
+++ pi_camera_ex1.py
@@ -0,0 +1,17 @@
+import picamera
+import time
+import subprocess
+
+with picamera.PiCamera() as cam:
+    for i in range(1,10):
+        filename = "test" + format(i, "03d") + ".jpg"
+        cam.capture(filename)
+    #subprocess.call(["raspistill", "-n", "-t", "1", "-w", "200",
+    #                "-h", "200", "-co", 
+    #                 "90", "-ifx", "sketch", "-e", "jpg",
+    #                 "-e", "jpg", "-o", filename])
+        print("taking photo", i)
+
+    print("Encoding...")
+    subprocess.call(["avconv", "-r", "5", "-i", "test%03d.jpg", "-r", "24", "-s", "200x200", 
+                "-vsync", "cfr" , "out.h264"])
